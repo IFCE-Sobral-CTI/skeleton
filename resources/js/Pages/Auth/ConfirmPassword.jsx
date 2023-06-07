f3f3aaf9b0792363ced6d7bcacd1cdb9dbdc9e28
Alan Freire <alan.freire.87@gmail.com>
2023-06-07 15:23:46 -0300

    Fix: translation

--- resources/js/Pages/Auth/ConfirmPassword.jsx
+++ resources/js/Pages/Auth/ConfirmPassword.jsx
@@ -32,7 +32,7 @@ export default function ConfirmPassword() {
             <Head title="Confirm Password" />
 
             <div className="mb-4 text-sm text-gray-600">
-                This is a secure area of the application. Please confirm your password before continuing.
+                Esta é uma área segura do aplicativo. Por favor, confirme sua senha antes de continuar.
             </div>
 
             <form onSubmit={submit}>
@@ -53,7 +53,7 @@ export default function ConfirmPassword() {
 
                 <div className="flex items-center justify-end mt-4">
                     <PrimaryButton className="ml-4" processing={processing}>
-                        Confirm
+                        Confirmar
                     </PrimaryButton>
                 </div>
             </form>
