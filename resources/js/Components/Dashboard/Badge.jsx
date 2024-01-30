471f2e4fbbb08d8a25d8c7d49336c73d61d6c45a
Alan Freire <alan.freire.87@gmail.com>
2024-01-30 13:41:14 -0300

    Fix: remove duplicate component

--- resources/js/Components/Dashboard/Badge.jsx
+++ resources/js/Components/Dashboard/Badge.jsx
@@ -1,4 +1,4 @@
-import React, { Children, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { twMerge } from 'tailwind-merge';
 
 export default function Badge({color, children, className}) {
